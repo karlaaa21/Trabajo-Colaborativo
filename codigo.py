c2dcf3b700b437f512315c1df847cf6196f740d7
javipiro <ja.valdesg@duocuc.cl>
2025-05-14 22:50:37 -0400

    continuacion del codigo

--- codigo.py
+++ codigo.py
@@ -4,3 +4,18 @@ num3= int(input("Ingrese el tercer numero "))
 
 contador_par= 0
 contador_impar= 0 
+
+if num1 % 2 == 0 :
+    contador_par += 1
+else :
+    contador_impar +=1
+
+if num2 % 2 == 0 :
+    contador_par += 1
+else :
+    contador_impar += 1
+
+if num3 % 2 == 0 :
+    contador_par += 1
+else:
+    contador_impar += 1
